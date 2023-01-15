1b5468d77e6e4e5176142bbb6d5e138120115036
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-15 20:25:41 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using HRApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Employees()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Employees(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddEmployee(employee);
+                return View("ConfirmPage", employee);
+            }
+            else
+                return View();
+        }
+
+        public ViewResult EmployeeData()
+        {
+            return View(Repository.GetEmployees().Where(e => e.isActive == true));
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRApplication.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Invalid Name")]
+        public string EmployeeName { get; set; }
+        [Required(ErrorMessage = "Please Select Department")]
+        public string Department { get; set; }
+        public bool isActive { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string JobTitle { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public string EmailAddres { get; set; }
+        [Required, DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
